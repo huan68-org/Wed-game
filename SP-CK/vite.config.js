833124442a149c65ff551b43b36048fa01e5012c
hunghel1oss <ffthelight@gmail.com>
2025-07-20 23:29:35 +0700

    refactor: centralize all config into vite.config.js

--- SP-CK/vite.config.js
+++ SP-CK/vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+        autoprefixer,
+      ],
+    },
+  },
   optimizeDeps: {
     esbuildOptions: {
       define: {
@@ -20,4 +30,4 @@ export default defineConfig({
       ],
     },
   },
-}) 
+})
