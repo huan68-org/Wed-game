1742fd72a976b63997c8e8d393372d6c2735b253
hunghel1oss <ffthelight@gmail.com>
2025-07-20 23:11:12 +0700

    fix: force resolve events module using esbuild polyfill

--- SP-CK/vite.config.js
+++ SP-CK/vite.config.js
@@ -1,15 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
+import tailwindcss from 'tailwindcss'
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
-          tailwindcss(),
+            tailwindcss()
   ],
-  resolve: {
-    alias: {
-      events: 'events/',
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis',
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        {
+          name: 'node-globals-polyfill',
+          setup(build) {
+            build.onResolve({ filter: /events/ }, (args) => {
+              return { path: require.resolve('events/') }
+            })
+          },
+        },
+      ],
     },
   },
 })
