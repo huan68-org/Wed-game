22169a6f994566e320e94c91688ca6034ebbc251
hunghel1oss <ffthelight@gmail.com>
2025-07-20 23:19:46 +0700

    fix: final configuration for deployment

--- SP-CK/vite.config.js
+++ SP-CK/vite.config.js
@@ -1,18 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react(),
-            tailwindcss()
-  ],
+  plugins: [react()],
   optimizeDeps: {
     esbuildOptions: {
-      // Node.js global to browser globalThis
       define: {
         global: 'globalThis',
       },
-      // Enable esbuild polyfill plugins
       plugins: [
         {
           name: 'node-globals-polyfill',
